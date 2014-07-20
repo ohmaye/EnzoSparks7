f9c5d83543d0b33ed2fc28c0ec49be75ab2152e3
Enio Ohmaye <enioohmaye@Enios-MacBook-2.local>
2014-07-20 15:17:36 +0800

    q

--- EnzoSparks7/GameViewController.swift
+++ EnzoSparks7/GameViewController.swift
@@ -24,25 +24,103 @@ extension SKNode {
     }
 }
 
+extension UIView {
+    func handlePanGesture( gestureRecognizer : UIPanGestureRecognizer ) {
+        let location = gestureRecognizer.locationInView(self.superview)
+        self.center = location
+        println("Called pan")
+    }
+}
+
 class GameViewController: UIViewController {
+    
+    var skView : SKView!
+    var skScene : GameScene!
+    var dictionary = Dictionary<UIView, SKEmitterNode>(minimumCapacity: 10)
+    var selectedView : UIView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
             // Configure the view.
-            let skView = self.view as SKView
+            skView = self.view as SKView
+            skView.multipleTouchEnabled = true
+            skScene = scene
+            
+            // Debug
             skView.showsFPS = true
             skView.showsNodeCount = true
             
             /* Sprite Kit applies additional optimizations to improve rendering performance */
             skView.ignoresSiblingOrder = true
             
+            // Test
+            
             /* Set the scale mode to scale to fit the window */
             scene.scaleMode = .AspectFill
             
             skView.presentScene(scene)
         }
+    }
+ 
+    override func touchesBegan(touches: NSSet!, withEvent event: UIEvent!) {
+        for touch: AnyObject in touches! {
+            let location = touch.locationInView(skView)
+            //createView(location)
+            println("Touched View: \(location)")
+            //super.touchesBegan(touches , withEvent: event )
+        }
+
+    }
+    
+    override func touchesMoved(touches: NSSet!, withEvent event: UIEvent!) {
+        for touch: AnyObject in touches! {
+            let location = touch.locationInView(skView)
+            //createView(location)
+            println("Touches Moved: \(location)")
+            //super.touchesBegan(touches , withEvent: event )
+        }
+        
+    }
+   
+    @IBAction func handlePanGestureInMainView( gestureRecognizer : UIGestureRecognizer ) {
+        let location = gestureRecognizer.locationInView(skView)
+        
+        var state = ""
+        switch gestureRecognizer.state {
+        case .Began:
+            state = "Began"
+            selectedView = createView(location)
+        case .Changed:
+            state = "Changed"
+            if let view = selectedView {
+                view.center = location
+            }
+        case .Ended, .Cancelled:
+            state = "Ended"
+            selectedView = nil
+        default:
+            state = "Unknown"
+        }
+        println("Called main view pan \(location) state: " + state)
+    }
+    
+    
+    func createView(location : CGPoint ) -> UIView {
+        let width: CGFloat = 50
+        let height: CGFloat = 50
+        let x = location.x - width / 2
+        let y = location.y - height / 2
+        let frame = CGRect(x: x, y: y, width: width, height: height)
+        var view = UIView(frame: frame)
+        // Add drag gesture recognizer
+        let gestureRecognizer = UIPanGestureRecognizer(target: view, action: "handlePanGesture:")
+        view.addGestureRecognizer(gestureRecognizer)
+        
+        view.backgroundColor = UIColor.whiteColor()
+        skView.addSubview(view)
+        return view
     }
 
     override func shouldAutorotate() -> Bool {
